be140d965b4523b07ae52cde44933d38142279e1
AndySmithQA <andy.smith@qa.com>
2023-02-21 14:30:21 +0000

    With Props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+import Card from './components/Card';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p><Card name="Bill" age="35" role="DevOps" isActive={true} /></p>
+      <p><Card name="Andy" age="27" role="Sales" isActive={false}/></p>
+      <p><Card name="Dave" age="64" role="Catering" isActive={true}/></p>
     </div>
   );
 }
-
 export default App;
